e630dae7c4edc9e601e4f45e753dd4e8b0029741
Hoàng <hoanghung20030@gmail.com>
2024-03-29 11:10:53 +0700

    config helper

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
@@ -9,145 +10,161 @@ use Illuminate\Support\Facades\DB;
 class Users extends Model
 {
     use HasFactory;
+
     protected $table = 'users';
+
     public function getAllUsers()
     {
-        $users = DB::select('SELECT * FROM users WHERE id');
+        $users = DB::select('SELECT * FROM users ORDER BY create_at DESC');
         return $users;
     }
+
     public function addUser($data)
     {
-        DB::insert('INSERT INTO users (fullname, email, create_at) values (?, ?,?)', $data);
-
+        DB::insert('INSERT INTO users (fullname, email, create_at) VALUES (?,?,?)', $data);
     }
+
     public function getDetail($id)
     {
         return DB::select('SELECT * FROM ' . $this->table . ' WHERE id = ?', [$id]);
     }
+
     public function updateUser($data, $id)
     {
-        $data[] = $id;
-        return DB::update('UPDATE ' . $this->table . ' SET fullname=?, email=?, update_at=? WHERE id = ?', $data);
+        $data = array_merge($data, [$id]);
+        return DB::update('UPDATE ' . $this->table . ' SET fullname = ?, email = ?, update_at = ? WHERE id = ?', $data);
     }
+
     public function deleteUser($id)
     {
-        return DB::delete('DELETE FROM ' . $this->table . ' WHERE id=?', [$id]);
+        return DB::delete("DELETE FROM $this->table WHERE id =?", [$id]);
     }
+
     public function statementUser($sql)
     {
         return DB::statement($sql);
     }
-    public function learningQueryBuilder()
+
+    public function learnQueryBuilder()
     {
         DB::enableQueryLog();
-        // lấy tất cả bản ghi
-        $id = 2;
-        // $lists = DB::table($this->table)
-        // dd($list);
-        // ->where('id','>=',1)->get();
-        // ->where('id','<>',19)->get();
-        // ->select('fullname as hoten', 'email', 'id','updated_at','created_at')
-        // ->where('id',1)
-        // ->where(function($query) use  ($id) {
-        //     $query ->where('id','<',$id)->orWhere('id','>',2);
-        // })
-        // ->where('fullname','like','%hoanghung%')
-        // ->whereBetween('id',[1,3])
-        // ->whereNotBetween('id',[1,3])
-        // ->whereDate('updated_at','2024-3-24')    
-        // ->whereMonth('updated_at','03')
-        // ->whereDate('updated_at','24')
-        // ->whereYear('updated_at','2024')
-        // ->whereColumn('updated_at','<>','created_at')
-        // ->whereIn('id',[1,3])
-        // ->whereNotIn('id',[1,3])
-        // ->whereNull('updated_at')
-        // ->whereNotNull('updated_at')
-        // ->where('id', 1)->orWhere('id',2)
-        // // ->get();
-        // ->toSql();
+        // $id = 2;
+        // $list = DB::table($this->table)
+        //     ->select('fullname as hoten', 'email', 'id', 'update_at','create_at')
+        // ->where('id', 'like', '%1%')
+        // ->whereNotIn('id', [1, 3])
+        // ->whereIn('id', [1, 3])
+        // ->whereNotBetween('id', [1, 3])
+        // ->whereBetween('id', [1, 3])
+        // ->whereNull('update_at')    
+        // ->whereNotNull('update_at')    
+        // ->whereYear('create_at', '2024')
+        // ->whereColumn('create_at', 'update_at') 
         // ->get();
+        // ->where('id', '>', 1)   
+        // ->where('id', '<>', 8)
+        // ->where('id', '>=', 8)
+        // ->where('id', '<=', 9)
+        // ->where([
+        //     // ['id',  '>=', 8],
+        //     // ['id',  '<=', 9],
+        //     'id'=> '8',
+        //     'fullname'=> 'Nguyễn Văn Tường Nu',
+        // ])
+        // ->where('id', 1)
+        // ->orWhere('id', 2)
+        // ->toSql();
+        // ->where('id', 0)
+        // // ->where('id','<',2)
+        // ->where(function ($query) use ($id) {
+        //     $query->where('id', '<', $id)->orWhere('id', '>', $id);
+        // })
 
-
-        //join bảng
-        // $list =DB::table('users')
-        // ->select('users.*', 'groups.name')
-        // ->rightJoin('groups', 'users.group_id', '=', 'groups_id')
-        // ->orderBy('id','desc')
-        // ->orderBy('created_at','asc')
+        //Join bang
+        // $list = DB::table('users')
+        // ->select('users.*', 'groups.name as group_name')
+        // ->rightJoin('groups','users.group_id','=','groups.id')
+        // ->orderBy('create_at', 'desc')
+        // ->orderBy('id', 'desc')
         // ->inRandomOrder()
-        // ->select(DB::raw('count(id) as email_count'),'email','fullname')
-        // ->groupBy('email')
+        // ->select(DB::raw('count(id) as email_count'), 'email', 'fullname')
+        // ->groupBy('email')  
+        // ->having('email_count', '>=', 1)
         // ->groupBy('fullname')
-        // ->having('email_count','>=',2)
         // ->limit(1)
         // ->offset(1)
         // ->take(2)
         // ->skip(2)
-        //     ->get();
+        // ->get();
 
-        // lấy bản ghi đầu tiên
-        // $list = DB::table($this->table)->first();
+        // insert data
+        // $status = DB::table('users')->insert([
+        //     'fullname' => 'Nguyễn Văn Tường Ne',
+        //     'email' => 'ne.nguyenvan25@student.passerellesnumeriques.org',
+        //     'group_id' => 1,
+        //     'create_at' => date('Y-m-d H:i:s'),
+        // ]);
 
-        $status = DB::table('users')->insert([
-            'fullname' => "Hoang huuhung",
-            'email' => "hoang@example",
-            'group_id' => 1,
-            'created_at' => date('Y-m-d H:i:s')
-        ]);
-
-        // dd($status);
         // $lastId = DB::getPdo()->lastInsertId();
+        // dd($lastId);
 
         // $lastId = DB::table('users')->insertGetId([
-        //     'fullname' =>"Hoang huuhung",
-        //     'email' =>"hoang@example",
-        //     'group_id'=>1,
-        //     'created_at'=>date('Y-m-d H:i:s')
+        //     'fullname' => 'Nguyễn Văn Tường Non',
+        //     'email' => 'non.nguyenvan25@student.passerellesnumeriques.org',
+        //     'group_id' => 1,
+        //     'create_at' => date('Y-m-d H:i:s'),
         // ]);
         // dd($lastId);
-        // $status = DB::table('users')
-        //     ->where('id', 2)
-        //     ->update([
-        //         'fullname' => "Hoang huuhung",
-        //         'email' => "hoang@example",
-        //         'group_id' => 1,
-        //         'created_at' => date('Y-m-d H:i:s')
-        //     ]);
 
-
+        // sua bang ghi
         // $status = DB::table('users')
-        // ->where('id', 2)
+        // ->where('id', 6)
+        // ->update([
+        //     'fullname' => 'Nguyễn Văn Tường Nen',
+        //     'email' => 'nen.nguyenvan25@student.passerellesnumeriques.org',
+        //     'update_at' => date('Y-m-d H:i:s'),
+        // ]);
+
+        //xoa bang ghi
+        // $status = DB::table('users')
+        // ->where('id', 6)
         // ->delete();
-        // dd($status);
 
-        // $count = DB::table('users')->wheres('id','>',20)->count();
+        //dem bang ghi
+        // $count = DB::table('users')
+        // ->where('id','>',3)
+        // ->count();
         // dd($count);
+
         $list = DB::table('users')
-        // ->selectRaw('email,  fullname count(id) as email_count')
-        // ->groupBy('fullname')
-        // ->where(DB::raw('id>2'))
-        // ->where('id','>',2)
-        // ->orWhereRaw('id>20')
-        // ->orderByRaw('email,fullname')
-        // ->having('email_count' , '>=',2)
-        // ->havingRaw('email_count >?',[2])
-        // ->where(
-        //     'group_id',
-        //     '=',
-        //     function ($query){
-        //         $query->select('id')
-        //         ->from('groups')
-        //         ->where('name', '=','Administrator');
-        //     }
-        // )
-        // ->select('email',DB::raw('(SELECT count(id) FROM `groups`) as group_count') )
-        // ->selectRaw('SELECT count(id) FROM `groups`) as group_count')
+            // ->selectRaw('fullname, email, count(id) as email_count')
+            // ->select(DB::raw('fullname, email'))
+            // ->groupBy('email')
+            // ->groupBy('fullname')
+            // ->where(DB::raw('id>2'))
+            // ->orWhereRaw('id>?', [`2`])
+            // ->orderByRaw('create_at DESC , update_at ASC')
+            // ->groupByRaw('fullname, email')
+            // ->havingRaw('email_count >= 1')
 
-        ->get();
+            // ->where(
+            //     'group_id',
+            //     '=',
+            //     function ($query) {
+            //         $query->select('id')
+            //             ->from('groups')
+            //             ->where('name', '=', 'Admin');
+            //     }
+            // )
+
+            // ->select('email', DB::raw('(SELECT count(id) FROM `groups`) as group_count'))
+            ->selectRaw('email, (SELECT count(id) FROM `groups`) as group_count')
+            ->get();
+
+        // dd($list);
+
         $sql = DB::getQueryLog();
-        dd($sql);
+        // dd($sql);
+        $detail = DB::table($this->table)->first();
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,65 @@
 <?php
 
-use App\Http\Controllers\CategoriesController;
-use App\Http\Controllers\Admin\ProductsController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UsersController;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can egister web routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/san-pham',[HomeController::class,'products'])->name('product');
-Route::get('/them-san-pham',[HomeController::class,'getAdd']);
-Route::post('/them-san-pham',[HomeController::class,'postAdd']);
-Route::put('/them-san-pham',[HomeController::class,'putAdd']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('users')->name('users.')->group(function(){
-    Route::get('/',[UsersController::class,'index'])->name('index');
-    Route::get('/add',[UsersController::class,'add'])->name('add');
-    Route::post('/add',[UsersController::class,'postAdd'])->name('post-add');
-    Route::get('/edit/{id}',[UsersController::class,'getEdit'])->name('edit');
-    Route::post('/update',[UsersController::class,'postEdit'])->name('post-edit');
-    Route::get('/delete/{id}',[UsersController::class,'delete'])->name('delete');
-
+Route::get('/san-pham', [HomeController::class, 'products'])->name('product');
+Route::get('/them-san-pham', [HomeController::class, 'getAdd']);
+Route::post('/them-san-pham', [HomeController::class, 'postAdd'])->name('post-add');
+// Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
+Route::get('/demo-response2', function (Request $request) {
+    return $request->cookie('Unicode', 'Training PHP');
 });
 
-// client route 
-// Route::prefix('categories')->group(function(){
-//     // danh sách chuyên mục
-//     Route::get('/',[CategoriesController::class,'index'])->name('categories.list');
-//     // lấy chi tiết 1 chuyên mục
-//     Route::get('/edit/{id}',[CategoriesController::class,'getCategory']);
-//     //xử lý hàm update chuyên mục
-//     Route::post('/edit/{id}',[CategoriesController::class,'updateCategory']);
-//     //hiển thị form add dữ liệu 
-//     Route::get('/add',[CategoriesController::class,'addCategory'])->name('categories.add');
-//     // xử lý thêm chuyên mục
-//     Route::post('/add',[CategoriesController::class,'handleAddCategory']);
-//     //xóa chuyên mục
-//     Route::delete('/delete/{id}',[CategoriesController::class,'deleteCategory'])->name('categories.delete');
+Route::get('/lay-thong-tin', [HomeController::class, 'getArr']);
+Route::get('/demo-response', function () {
+    // $content = json_decode(['Item1', 'Item2', 'Item3']);
+    // $response = (new Response($content))->header('Content-Type', 'application/json');
+    // return $response;
+    // $response = (new Response())->cookie('Unicode', 'Trainning PhP');
+    // return $response;
+    // $contentArr = ['name'=> 'Unicode',
+    //                 'version' => '10.x',
+    //                 'lesson' => 'HTTP response Laravel'   
+    //             ];
+    // return response()->json($contentArr, 404)->header("Api-Key",'1234');
+    // return view('clients.demo-test');
+    return view('clients.demo-test');
 
-//     //getfile
-//     Route::get('/upload',[CategoriesController::class,'getFile']);
+})->name('demo-response');
 
-//     //xử lý file upoload
-//     Route::post('/upload',[CategoriesController::class,'handleFile'])->name('categories.upload');
-// });
+Route::post('/demo-response', function (Request $request) {
+    if(!empty($request->username)){
+        return back()->withInput()->with('mess', 'Valida thanh cong');
+    }
+    return redirect(route('demo-response'))->with('mess', 'Valida ko thanh cong');
+});
 
-// Route::get('san-pham/{id}',[HomeController::class,'getProductDetails']);
-// //admin route
-// Route::middleware('auth.admin')->prefix('admin')->group(function(){
-//     Route::get('/dashboard',[DashboardController::class,'index']);
-//     Route::resource('products',ProductsController::class);
-// });
+Route::get('/download-image', [HomeController::class, 'downloadImage'])->name('download-image');
+Route::get('/download-doc', [HomeController::class, 'downloadDoc'])->name('download-doc');
+
+Route::prefix('users')->name('users.')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('index');
+
+    Route::get('/add', [UserController::class, 'add'])->name('add');
+    Route::post('/add', [UserController::class, 'postAdd'])->name('post-add');
+
+    Route::get('/edit/{id}', [UserController::class, 'getEdit'])->name('edit');
+    Route::post('/update', [UserController::class, 'postEdit'])->name('post-edit');
+    Route::get('/delete/{id}', [UserController::class, 'delete'])->name('delete');
+});
