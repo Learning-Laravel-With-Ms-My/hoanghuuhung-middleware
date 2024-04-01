03203c34540879aafa15a8e4147cb6b912f1f94d
Hoàng <hoanghung20030@gmail.com>
2024-04-01 19:13:39 +0700

    bai 45

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,10 +12,40 @@ class PostController extends Controller
         // dd($allPost);
         // $post  = Post::find('c1');
         // dd($post);
-        $post = new Post;
-        $post->title = "bai viet 3";
-        $post->content = "noi dung bai vbiet 3";
-        $post->status = 1;
-        $post->save();
+        // $post = new Post;
+        // $post->title = "bai viet 3";
+        // $post->content = "noi dung bai vbiet 3";
+        // $post->status = 1;
+        // $post->save();
+
+        echo '<h2>Post ORM</h2>';
+        // $allPost = Post::all();
+        // if($allPost->count() > 0){
+        //     foreach($allPost as $post){
+        //         echo '<h2>' . $post->title . '</h2>';
+        //     }
+        // }
+            // $detail = Post::find(1);
+            // dd($detail);
+        // $activePost = Post::where('status',1)->orderBy('id','DESC')->get();
+    
+        // dd($activePost);
+        
+        // $allPost = Post::all();
+        // $activePost = $allPost->reject(function($post){
+        //     return $post->status =0;
+        // });
+
+        // dd($activePost);
+        Post::chunk(2, function ($posts){
+            foreach($posts as $post){
+                echo '<h2>' . $post->title . '</h2>';
+            }
+            echo 'ket thuc chuk';
+        });
+        $allPost = Post::where('status',1)->cursor();
+        foreach ($allPost as $post){
+            echo $post->title;
+        }
     }
 }
